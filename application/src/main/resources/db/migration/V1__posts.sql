1601e1a176a38e7b1332dfd0c791be7658d45f5d
Dylan <okaa.pii@gmail.com>
2020-02-14 09:36:01 +0100

    Flyway + clean

---
+++ application/src/main/resources/db/migration/V1__posts.sql
@@ -0,0 +1,5 @@
+create table posts (
+    id uuid primary key,
+    title text not null,
+    content text not null
+)
--- infrastructure/src/main/kotlin/com/okaapi/kotlindemo/model/PostEntity.kt
+++ infrastructure/src/main/kotlin/com/okaapi/kotlindemo/model/PostEntity.kt
@@ -1,15 +1,20 @@
 package com.okaapi.kotlindemo.model
 
+import java.util.*
 import javax.persistence.Entity
 import javax.persistence.GeneratedValue
 import javax.persistence.Id
+import javax.persistence.Table
 
 @Entity
+@Table(name = "posts")
 class PostEntity(
-        @Id @GeneratedValue var id: Long?,
         var title: String,
-        var content: String) {
+        var content: String,
+        @Id @GeneratedValue var id: UUID? = null) {
 
     fun toPost() = Post(title, content)
 
 }
+
+fun Post.toEntity() = PostEntity(title = title, content = content)

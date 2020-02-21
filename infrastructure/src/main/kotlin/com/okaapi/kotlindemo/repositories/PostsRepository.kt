3ec3c99ee36c1cb49496a32dc4cfff3111585612
Dylan <okaa.pii@gmail.com>
2020-02-21 11:17:08 +0100

    Update PostsRepository.kt

--- infrastructure/src/main/kotlin/com/okaapi/kotlindemo/repositories/PostsRepository.kt
+++ infrastructure/src/main/kotlin/com/okaapi/kotlindemo/repositories/PostsRepository.kt
@@ -2,5 +2,6 @@ package com.okaapi.kotlindemo.repositories
 
 import com.okaapi.kotlindemo.model.PostEntity
 import org.springframework.data.jpa.repository.JpaRepository
+import java.util.*
 
 interface PostsRepository : JpaRepository<PostEntity, UUID>
